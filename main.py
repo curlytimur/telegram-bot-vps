2794376919adf2c71786bd059ce507577c8c3364
pc <e>
2022-07-28 16:16:15 +0300

    add button generation

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def get_data():
     sell_price = response["btc_usd"]["sell"]
     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price:{sell_price}")
 
+
+BUTTONS = ["BTC", "ETH", "XRP"]
+
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
 
@@ -20,33 +23,42 @@ def telegram_bot(token):
     def str_hi(hi):
 
         keyboard = types.ReplyKeyboardMarkup()
-        button_1 = types.KeyboardButton(text="price BTC")
-        keyboard.add(button_1)
-        button_3 = types.KeyboardButton(text="price ETH")
-        keyboard.add(button_3)
-        button_2 = "help"
-        keyboard.add(button_2)
+        for button_text in BUTTONS:
+            button = types.KeyboardButton(text=f"price {button_text}")
+            keyboard.add(button)
+        # button_1 = types.KeyboardButton(text="price BTC")
+        # keyboard.add(button_1)
+        # button_4 = types.KeyboardButton(text="price BTC")
+        # keyboard.add(button_4)
+        # button_3 = types.KeyboardButton(text="price ETH")
+        # keyboard.add(button_3)
+        # button_2 = "help"
+        # keyboard.add(button_2)
         bot.send_message(hi.chat.id, "Hello friend! Write the 'price' to find out the cost of BTC or ETH", reply_markup=keyboard)
 
-    @bot.message_handler(content_types=["text"])
+    def check_message(msg):
+        msg_text = msg.text
+        return msg_text[:6] == "price " and len(msg_text) == 9
+
+    @bot.message_handler(content_types=["text"], func=check_message)
     def send_text(message):
-        if message.text.lower() == "price btc":
-            try:
-                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
-                response = req.json()
-                sell_price = response["btc_usd"]["sell"]
-                bot.send_message(
-                    message.chat.id,
-                    f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price:{sell_price}"
-                )
-            except Exception as ex:
-                print(ex)
-                bot.send_message(
-                    message.chat.id,
-                    "Damn... Somthing was wrong..."
-                )
-        else:
-            bot.send_message(message.chat.id, "What???")
+        print(f"Received message: {message.text}")
+        try:
+            ticker = message.text.lower()[6:]
+            url = f"https://yobit.net/api/3/ticker/{ticker}_usd"
+            req = requests.get(url)
+            response = req.json()
+            sell_price = response[f"{ticker}_usd"]["sell"]
+            bot.send_message(
+                message.chat.id,
+                f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell {ticker.upper()} price:{sell_price}"
+            )
+        except Exception as ex:
+            print(ex)
+            bot.send_message(
+                message.chat.id,
+                "Damn... Somthing was wrong..."
+            )
 
     bot.polling()
 
